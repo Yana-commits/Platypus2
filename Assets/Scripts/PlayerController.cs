190c3e41a94bd9a589d8741ba94ce2ad41cdad5d
Yana-commits <74359419+Yana-commits@users.noreply.github.com>
2021-11-03 10:23:19 +0200

    player_controller for smooth move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,44 +6,55 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
     
-    private int lineToMove = 1;
-    private float lineDistance ;
+    //private int lineToMove = 1;
+    //private float lineDistance ;
+    private float horizontalInput;
 
     public Animator animator;
     public float speed;
+
     private void Update()
     {
-        if (SwipeController.swipeRight)
-        {
-            if (lineToMove < 2)
-                lineToMove++;
-        }
+        //if (SwipeController.swipeRight)
+        //{
+        //    if (lineToMove < 2)
+        //        lineToMove++;
+        //}
 
-        if (SwipeController.swipeLeft)
-        {
-            if (lineToMove > 0)
-                lineToMove--;
-        }
+        //if (SwipeController.swipeLeft)
+        //{
+        //    if (lineToMove > 0)
+        //        lineToMove--;
+        //}
 
-        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
-        if (lineToMove == 0)
-            targetPosition += Vector3.left * lineDistance;
-        else if (lineToMove == 2)
-            targetPosition += Vector3.right * lineDistance;
+        //Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        //if (lineToMove == 0)
+        //    targetPosition += Vector3.left * lineDistance;
+        //else if (lineToMove == 2)
+        //    targetPosition += Vector3.right * lineDistance;
 
-        transform.position = targetPosition;
+        //transform.position = targetPosition;
     }
 
     void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed;
-        rigidbody.MovePosition(rigidbody.position + forwardMove);
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * 0.03f;
+        rigidbody.MovePosition(rigidbody.position + forwardMove + horizontalMove);
+    }
+    private void GetDirection(Vector2 direction)
+    {
+        horizontalInput = direction.x;
     }
 
     public void Init(PlayerSettings settings)
     {
+        SwipeDetection.SwipeEvent += GetDirection;
         speed = settings.speed;
-        lineDistance = settings.lineDistance;
+        //lineDistance = settings.lineDistance;
     }
-    
+    private void OnDestroy()
+    {
+        SwipeDetection.SwipeEvent -= GetDirection;
+    }
 }
